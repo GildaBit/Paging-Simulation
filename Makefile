ff9e55df87b1a89632d4e28757a1c76140fe2c3c
Gilad Bitton <bitton.gilad@gmail.com>
2025-10-26 16:53:08 -0700

    added parsing functionality and base makefile

--- Makefile
+++ Makefile
@@ -3,26 +3,14 @@
 # Makefile for Paging Assignment 3
 CC = g++
 CFLAGS = -std=c++17
-OBJS = main.o sharedData.o scheduler.o process.o log.o
-TARGET = schedule
+OBJS = main.o
+TARGET = pagingwithpr
 
 $(TARGET): $(OBJS)
 	$(CC) $(CFLAGS) -o $(TARGET) $(OBJS)
 
-main.o: main.cpp sharedData.h scheduler.h process.h log.h
+main.o: main.cpp 
 	$(CC) $(CFLAGS) -c main.cpp
-
-process.o: process.cpp process.h
-	$(CC) $(CFLAGS) -c process.cpp
-
-sharedData.o: sharedData.cpp sharedData.h process.h
-	$(CC) $(CFLAGS) -c sharedData.cpp
-
-scheduler.o: scheduler.cpp scheduler.h sharedData.h process.h
-	$(CC) $(CFLAGS) -c scheduler.cpp
-
-log.o: log.cpp log.h
-	$(CC) $(CFLAGS) -c log.cpp
 
 clean:
 	rm -f $(OBJS) $(TARGET)
--- main.cpp
+++ main.cpp
@@ -21,7 +21,7 @@ int main(int argc, char** argv) {
     vector<int> levelBits; // Number of bits for each level in the multi-level page
 
 
-    //parse command line arguments
+    //parse optional command line arguments
     while ((opt = getopt(argc, argv, "n:f:b:l:")) != -1) {
         switch (opt) {
             case 'n':
@@ -48,43 +48,55 @@ int main(int argc, char** argv) {
             case 'l':
                 logMode = optarg;
                 break;
-        }
-    }
-    if (optind >= argc) {
-        cout << "Usage: " << argv[0] << " <input_file> [-s strategy] [-q quantum]\n";
-        exit(0);
-    }
-
-    // get text file and check if it opens and exists
-    string input_file = argv[optind];
-    ifstream infile(input_file);
-    if (!infile.is_open() || infile.fail()) {
-        cout << "Unable to open " << input_file << endl;
-        exit(0);
-    }
-
-    vector<vector<int>> processes;
-    string line;
-
-    // parse input file for the processes
-    while(getline(infile, line)) {
-        if(line.empty()) continue;
-        istringstream iss(line);
-        vector<int> process;
-        int burst;
-        while (iss >> burst) {
-            if (burst <= 0) {
-                cout << "A burst number must be bigger than 0" << endl;
+            default:
+                cerr << "Usage: " << argv[0]
+                     << " [-n numAccesses] [-f availFrames] [-b bitUpdateInterval] [-l logMode] trace.tr <levelBits...>" << endl;
                 exit(0);
-            }
-            process.push_back(burst);
         }
-        if (process.size() % 2 == 0) {
-            cout << "There must be an odd number of bursts for each process" << endl;
+    }
+
+    // Remaining (mandatory) args: trace file and level bits 
+    if (optind >= argc) {
+        cerr << "Usage: " << argv[0]
+             << " [-n numAccesses] [-f availFrames] [-b bitUpdateInterval] [-l logMode] trace.tr <levelBits...>" << endl;
+        exit(0);
+    }
+
+    // sets name of trace file and then increments optind to point to next arg
+    string traceFile = argv[optind++];
+
+    // check if trace file opens and exists
+    ifstream infile(traceFile);
+    if (!infile.is_open() || infile.fail()) {
+        cerr << "Unable to open " << traceFile << endl;
+        exit(0);
+    }
+
+    // parse remaining args: level bits
+    int totalBits = 0;
+    for (int i = optind, level = 0; i < argc; i++, level++) {
+        int bits = atoi(argv[i]);
+        if (bits < 1) {
+            cerr << "Level " << level << " page table must be at least 1 bit" << endl;
             exit(0);
         }
-        processes.push_back(process);
+        totalBits += bits;
+        levelBits.push_back(bits);
     }
+
+    // total number of bits used in all page tables combined cannot exceed 28
+    if (totalBits > 28) {
+        cerr << "Too many bits used in page tables" << endl;
+        exit(0);
+    }
+
+    // testing
+    cout << "Trace file: " << traceFile << endl;
+    cout << "Levels: ";
+    for (int b : levelBits) cout << b << " ";
+    cout << endl;
+    cout << "n=" << numAccesses << ", f=" << availFrames 
+         << ", b=" << bitUpdateInterval << ", l=" << logMode << endl;
 
     return 0;
 }
